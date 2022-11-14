4b4a04b9aeefb653f9685c5ad0acd754d04aeb00
Adriano Coins Git <adrianodemedeiros@gmail.com>
2022-11-14 10:01:10 -0300

    MinhaClasse

---
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -0,0 +1,14 @@
+public class MinhaClasse {
+    public static void main(String[] args) throws Exception {
+        String primeiroNome = "Gleyson";
+        String segundoNome = "Sampaio";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
