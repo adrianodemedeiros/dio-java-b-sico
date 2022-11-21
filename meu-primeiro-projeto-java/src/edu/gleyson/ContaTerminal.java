b3325bd2a954ac4704351be38ab509ae5e833849
Adriano Coins Git <adrianodemedeiros@gmail.com>
2022-11-21 19:12:38 -0300

    Desafio Proposto

---
+++ meu-primeiro-projeto-java/src/edu/gleyson/ContaTerminal.java
@@ -0,0 +1,12 @@
+package edu.gleyson;
+
+public class ContaTerminal {
+    //TODO: Conhecer e importar a classe Scanner
+
+    //Exibir as mensagens para o nosso usuário
+
+
+    //Obter pela scanner os valores digitados no terminal
+
+    //Exibir a mensagem conta criada
+}
